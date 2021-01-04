b8c33dd795e0dba8caa80674f47717ab2a2157e0
Pyry <vuorelapyry@gmail.com>
2021-01-04 23:12:00 +0200

    Add LoginPage

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LoginPage from './views/LoginPage';
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+ReactDOM.render(<LoginPage/>, document.getElementById('root'));
---
+++ src/views/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+const LoginPage: FunctionComponent = () => {
+    return(
+        <h1>Login Page</h1>
+    );
+};
+
+export default LoginPage;
